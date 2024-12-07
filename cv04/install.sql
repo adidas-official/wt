f1e7d167895d659f47af6ffb85981779683998f3
zfrydryn <frydryn@student.vspj.cz>
2024-12-07 22:08:58 +0100

    updated install.sql for cv04

--- cv04/install.sql
+++ cv04/install.sql
@@ -1,28 +1,30 @@
 -- Active: 1728646102576@@127.0.0.1@53306@wt
 
-CREATE TABLE `names` (
+DROP TABLE IF EXISTS `wt_04_names`;
+
+CREATE TABLE `wt_04_names` (
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` VARCHAR(32) NOT NULL
     );
 
-INSERT INTO `names` (`name`) VALUES ("Alice");
-INSERT INTO `names` (`name`) VALUES ("Benjamin");
-INSERT INTO `names` (`name`) VALUES ("Catherine");
-INSERT INTO `names` (`name`) VALUES ("Daniel");
-INSERT INTO `names` (`name`) VALUES ("Edward");
-INSERT INTO `names` (`name`) VALUES ("Fiona");
-INSERT INTO `names` (`name`) VALUES ("George");
-INSERT INTO `names` (`name`) VALUES ("Hannah");
-INSERT INTO `names` (`name`) VALUES ("Isabelle");
-INSERT INTO `names` (`name`) VALUES ("Jack");
-INSERT INTO `names` (`name`) VALUES ("Katherine");
-INSERT INTO `names` (`name`) VALUES ("Liam");
-INSERT INTO `names` (`name`) VALUES ("Michael");
-INSERT INTO `names` (`name`) VALUES ("Natalie");
-INSERT INTO `names` (`name`) VALUES ("Oliver");
-INSERT INTO `names` (`name`) VALUES ("Peter");
-INSERT INTO `names` (`name`) VALUES ("Quinn");
-INSERT INTO `names` (`name`) VALUES ("Rachel");
-INSERT INTO `names` (`name`) VALUES ("Samuel");
-INSERT INTO `names` (`name`) VALUES ("Victoria");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Alice");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Benjamin");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Catherine");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Daniel");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Edward");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Fiona");
+INSERT INTO `wt_04_names` (`name`) VALUES ("George");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Hannah");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Isabelle");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Jack");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Katherine");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Liam");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Michael");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Natalie");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Oliver");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Peter");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Quinn");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Rachel");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Samuel");
+INSERT INTO `wt_04_names` (`name`) VALUES ("Victoria");
 
