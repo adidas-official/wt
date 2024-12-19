545465f354fdd0df8fdcc63ee53b0dd72f423140
zfrydryn <frydryn@student.vspj.cz>
2024-12-19 21:04:50 +0100

    Ukol 9

--- cv09/ukol9/src/Controller/HomeController.php
+++ cv09/ukol9/src/Controller/HomeController.php
@@ -15,4 +15,30 @@ class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/poslipole', name: 'poslipole')]
+    public function posliPole(): Response
+    {
+        $pole = ['jedna', 'dva', 'tri', 'ctyri', 'pet'];
+        return $this->render('home/posliPole.html.twig', [
+            'pole' => $pole,
+        ]);
+    }
+
+    #[Route('/poslipolevpoli', name: 'poslipolevpoli')]
+    public function posliPoleVPoli(): Response
+    {
+        $lide = [
+            ['jmeno' => 'Pepa', 'prijmeni' => 'Novak', 'vek' => 25],
+            ['jmeno' => 'Jana', 'prijmeni' => 'Cernakova', 'vek' => 30],
+            ['jmeno' => 'Karel', 'prijmeni' => 'Svoboda', 'vek' => 35],
+            ['jmeno' => 'Marie', 'prijmeni' => 'Kovarova', 'vek' => 40],
+            ['jmeno' => 'Jiri', 'prijmeni' => 'Dvorak', 'vek' => 45],
+        ];
+
+        return $this->render('home/posliPoleVPoli.html.twig', [
+            'lide' => $lide,
+        ]);
+        
+    }
 }
