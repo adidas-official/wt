0e32ae26c304f794dc740168c4942a9af5d4d715
zfrydryn <frydryn@student.vspj.cz>
2024-12-21 20:39:16 +0100

    Home controller init
    This is an inital setup for home controller with defined logout route
    and home directory

---
+++ cv11/ukol11-security/src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): Response
+    {
+        return $this->render('home/logout.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
